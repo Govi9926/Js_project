f2df7d489e0d0b7aaf6511d50d64c79f03908fce
Govi9926 <govindgupta78090@gmail.com>
2025-03-29 20:50:13 +0530

    g

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+// const num1 = Math.ceil(Math.random() * 10);
+// const num2 = Math.ceil(Math.random()* 10);
+// let btn2 = document.getElementById('gg')
+
+
+
+// const bt3 = document.getElementById('btn3');
+// bt3.addEventListener('click',(e)=>{
+//     console.log('gggggg')
+// })
+
+
+// input = document.getElementById('input');
+// console.log(input)
+
+
+
+// const qt2 = document.getElementById('question');
+// qt2.innerText = `What is ${num1} multiply by ${num2}?`;
+
+// submit  = document.getElementByClass('btn');
+// submit.addEventListener('click',(e)=>{
+//     console.log(input)
+// })
