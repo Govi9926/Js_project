4192c78b78529e57f0676307ae0acd750832daff
Govi9926 <govindgupta78090@gmail.com>
2025-04-02 00:10:45 +0530

    gg

--- script.js
+++ script.js
@@ -17,6 +17,8 @@
 
 
 
+
+
 // const qt2 = document.getElementById('question');
 // qt2.innerText = `What is ${num1} multiply by ${num2}?`;
 
@@ -24,3 +26,26 @@
 // submit.addEventListener('click',(e)=>{
 //     console.log(input)
 // })
+
+
+const btnpc = document.getElementById('cp');
+
+btnpc.addEventListener('click', (e) => {
+    console.log('ff'); // Debugging message
+    window.location.href = 'https://www.linkedin.com/in/govind-gupta-a099a714a/';
+});
+btnpc.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/govind-gupta-a099a714a/', '_blank');
+});
+
+
+const about = document.getElementById('about');
+about.addEventListener('click', (e) => {
+    console.log('ff'); // Debugging message
+    document.getElementById('about_profile').innerHTML = 'Govind Gupta';
+    
+});
+
+
+
+
